e94f123f25c343c4246419a33101dbf85e448845
denne <denne@DESKTOP-OIHJ20M>
2021-08-06 20:23:46 +0800

    java

---
+++ spring/src/main/java/com/study/spring/case01/MainTest.java
@@ -0,0 +1,23 @@
+package com.study.spring.case01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainTest {
+	public static void main(String[] args) {
+		ApplicationContext  ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//Author author =ctx.getBean(Author.class);
+		Author author1 = (Author) ctx.getBean("author1");
+		Author author2 = (Author) ctx.getBean("author2");
+		Author author3 = (Author) ctx.getBean("author3");
+		Author author4 = (Author) ctx.getBean("author4");
+
+
+		//author1.setName("John");
+		System.out.println(author1);
+		System.out.println(author2);
+		System.out.println(author3);
+		System.out.println(author4);
+
+	}
+}
